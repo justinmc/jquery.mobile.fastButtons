a6a3e89fb6060a6732c8ad29d58aab27458d524f
Justin <justin@smartots.com>
2012-10-23 16:19:40 +0800

    adding final version of file

---
+++ jquery.mobile.fastButtons.js
@@ -0,0 +1,27 @@
+// jquery.mobile.fastButtons.js
+// Justin McCandless
+// justinmccandless.com
+// jQuery 1.6.4+
+// jQueryMobile 1.0.1+
+
+// This jQuery Mobile plugin makes click events faster, especially iOS
+// It does this by simply replacing 'click' events on document with 'vclick'
+
+var fastButtons = {
+	
+	replace: function() {
+		// copy the current click events on document
+		var clickEvents = jQuery.hasData( document ) && jQuery._data( document ).events.click;
+		clickEvents = jQuery.extend(true, {}, clickEvents);
+		
+		// remove these click events
+		$(document).off('click');
+	
+		// reset them as vclick events
+		for (var i in clickEvents) {
+			$(document).on('vclick', clickEvents[i].handler);
+		}
+	}
+};
+
+fastButtons.replace();
